14f1c726c2adc4623b0841609ed634db5e1e83c8
Clem6951 <115093134+Clem6951@users.noreply.github.com>
2022-12-15 16:15:25 +0100

    Add files via upload

--- Jeu/src/main/java/prefab/entity/Commercant1.java
+++ Jeu/src/main/java/prefab/entity/Commercant1.java
@@ -2,6 +2,7 @@ package prefab.entity;
 
 import java.util.HashMap;
 
+import engine.Cmd;
 import prefab.information.Position;
 import prefab.information.State;
 import java.awt.image.BufferedImage;
@@ -11,21 +12,47 @@ import java.awt.image.BufferedImage;
  * (Nom à changer)
  */
 public class Commercant1 extends Character implements Merchant{
+	
+	protected int ligne;
 
-    public Commercant1(Position position, HashMap<State, BufferedImage> graphics, String objectName, int horizontalHitBox, int verticalHitBox) {
+    public Commercant1(Position position, HashMap<State, BufferedImage> graphics, String objectName, int horizontalHitBox, int verticalHitBox, int ligne) {
         super(position, graphics, objectName, horizontalHitBox, verticalHitBox);
+        state =  State.IDLE_DOWN;
+        this.ligne = ligne;
     }
 
     @Override
-    public String getDialogue() {
-        return null;
+    public int getDialogue(int i) {
+    	switch (i) {
+    	
+    	case 1:
+    		System.out.println("Bonjour, voici le tout premier dialogue du jeu. \n");
+    		i = i+1;
+    		break;
+    	
+    	case 2:
+    		System.out.println("Pour continuer le dialogue, rentrez encore en collision avec moi. \n");
+    		i = i+1;
+    		break;
+    	
+    	case 3:
+    		System.out.println("C'est tout ce que j'avais � enseigner, le dialogue va maintenant se r�p�ter. \n");
+    		i = 1;
+    		break;
+    	}
+    	return (i);
     }
 
     @Override
     protected void initCharacteristic() {
         
     }
-
+    
+    @Override
+    public void objectUse(Player user, Cmd cmd) {
+    	this.ligne = getDialogue(this.ligne);
+    }
+    
     @Override
     public void die() {
         
